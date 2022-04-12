e209d73de71bbc0c5fb7ced6405fa76dd9e3fd16
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-04-12 11:07:02 +0100

    Try to get refmaps working and fail

--- src/main/java/me/mcblueparrot/mixintemplate/mixin/MixinMinecraft.java
+++ src/main/java/me/mcblueparrot/mixintemplate/mixin/MixinMinecraft.java
@@ -4,6 +4,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
@@ -11,6 +14,11 @@ public class MixinMinecraft {
 	@Overwrite
 	private String createTitle() {
 		return "Mixin Client 1.0.0 | Minecraft 1.18.2";
+	}
+
+	@Inject(method = "getInstance", at = @At("HEAD"))
+	private static void preGetInstance(CallbackInfoReturnable<Minecraft> cir) {
+		System.out.println("Hello");
 	}
 
 }
